f8b130296f498b08ac0b5bc4ed632dd28008eb73
rodrigocuccia <103201899+rodrigocuccia@users.noreply.github.com>
2024-08-30 01:31:27 -0300

    Update combinacoes.sh

--- combinacoes.sh
+++ combinacoes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Funcao pra calcular fatorial
-factorial() {
+fatorial() {
     n=$1
     fact=1
     while [ $n -gt 1 ]; do
@@ -15,7 +15,7 @@ n=19
 k=15
 
 # Calculando n! / (k! * (n-k)!)
-combinacaoes=$(echo "scale=0; $(factorial $n) / ($(factorial $k) * $(factorial $(($n - $k))))" | bc)
+combinacaoes=$(echo "scale=0; $(fatorial $n) / ($(fatorial $k) * $(fatorial $(($n - $k))))" | bc)
 
 echo "Numero total de combinacoes: $combinacoes"
 
